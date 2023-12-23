a2676f0cf6a4b49453d7f4c7e250798e2f23447e
valatwork <giardini.val@bcg.com>
2023-12-23 14:57:06 +0100

    Empty Commit

---
+++ 026-if-else-conditional-statement/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+var userChoice = Console.ReadLine();
+
+//if (userChoice == "ABC")
+//{
+//    Console.WriteLine("User typed ABC");
+//}
+//else
+//{
+//    Console.WriteLine( "User did not type ABC");
+//}
+
+if (userChoice.Length <= 3)
+{
+    Console.WriteLine("Short answer");
+}
+// else is only checked if the condition for if has not been met
+else if (userChoice.Length < 10 )
+{
+    Console.WriteLine("Medium answer");
+}
+else
+{
+    Console.WriteLine("Long answer");
+}
+
+
+Console.ReadKey();
