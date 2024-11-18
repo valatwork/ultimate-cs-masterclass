2112c31a2a469516a8eab3f77fb426c6784061e6
valatwork <valfuindor@outlook.com>
2024-11-18 21:32:02 +0100

    Empty commit

---
+++ 038-string-interpolation/StringInterpolation.cs
@@ -0,0 +1,28 @@
+﻿/*Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");*/
+
+// var userChoice = Console.ReadLine();
+
+int a = 4, b = 2, c = 10;
+Console.WriteLine("The first is:" + a + ", the second is: " + b + ", the third is: " + c);
+
+Console.WriteLine($"The first is: {a}, the second is: {b}, the third is: {c}");
+Console.WriteLine($"Sum: {a + b + c}");
+
+Console.ReadKey();
+
+
+/*Using string interpolation, implement the FormatDate method, 
+ which given three integers representing the year, 
+ month and day shall return a string in the format YEAR/MONTH/DAY.
+For example:
+for the year 2022, month 10, and day 29, it shall return "2022/10/29"
+for the year 1911, month 1, and day 5, it shall return "1911/1/5"*/
+
+
+public static string FormatDate(int year, int month, int day)
+{
+    return $"{year}/{month}/{day}";
+}
---
+++ 039-switch-statement/SwitchStatement.cs
@@ -0,0 +1,114 @@
+﻿
+Console.WriteLine("Hello, World!");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+var userChoice = Console.ReadLine();
+
+void PrintSelectedOption(string selectedOption)
+{
+    Console.WriteLine("Selected option: " + selectedOption);
+}
+
+/*switch (userChoice)
+{
+    case "S":
+        PrintSelectedOption("See all TODOs");
+        break;
+    case "A":
+        PrintSelectedOption("Add a TODO");
+        break;
+    case "R":
+        PrintSelectedOption("Remove a TODO");
+        break;
+    case "E":
+        PrintSelectedOption("Exit");
+        break;
+}
+
+Console.ReadKey();*/
+
+switch (userChoice)
+{
+    case "S":
+        PrintSelectedOption("See all TODOs");
+        break;
+    case "A":
+    case "a":
+        PrintSelectedOption("Add a TODO");
+        break;
+    case "R":
+        PrintSelectedOption("Remove a TODO");
+        break;
+    case "E":
+        PrintSelectedOption("Exit");
+        break;
+    default: Console.WriteLine("Invalid choice");
+        break;
+}
+
+Console.ReadKey();
+
+
+/*string ConvertPointsToGrade(int points)
+{
+    string result;
+    switch(points)
+    {
+        case 10:
+        case 9:
+            result = "A";
+            break;
+        case 8:
+        case 7: 
+        case 6:
+            result = "B";
+            break;
+        case 5: 
+        case 4:
+        case 3: 
+            result = "C";
+            break;
+        case 2:
+        case 1:  
+            result = "D";
+            break;
+        case 0:
+            result = "E";
+            break;
+        default:
+            result = "!";
+            break;
+
+    }
+    return result;
+}*/
+
+
+string ConvertPointsToGrade(int points)
+{
+    string result;
+    switch (points)
+    {
+        case 10:
+        case 9:
+            return "A";
+        case 8:
+        case 7:
+        case 6:
+            return "B";
+        case 5:
+        case 4:
+        case 3:
+            return "C";
+        case 2:
+        case 1:
+            return "D";
+        case 0:
+            return "E";
+        default:
+            return "!";
+    }
+}
