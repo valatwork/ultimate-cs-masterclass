2f6f69d19886740c78d2fd00ebfe6ea7da9ee330
valatwork <valfuindor@outlook.com>
2024-11-18 21:39:09 +0100

    Empty commit

---
+++ TodoList/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");*/
+
+// var userChoice = Console.ReadLine();
+
+int a = 4, b = 2, c = 10;
+Console.WriteLine("The first is:" + a + ", the second is: " + b + ", the third is: " + c);
+
+Console.WriteLine($"The first is: {a}, the second is: {b}, the third is: {c}");
+Console.WriteLine($"Sum: {a + b + c}");
+
+Console.ReadKey();
