dfd11ca71627581121fd43e042253fd3da8a79da
valatwork <valfuindor@outlook.com>
2024-11-19 21:40:32 +0100

    Empty commit

---
+++ 044-while-loop-2/044-while-loop-2/WhileLoopExercise.cs
@@ -0,0 +1,52 @@
+﻿namespace _044_while_loop_2;
+
+/*
+ Using the while loop, implement the CalculateSumOfNumbersBetween method,
+ which given two integers called firstNumber and lastNumber,
+ should calculate the sum of numbers between them (including the numbers themselves).
+
+
+If lastNumber is smaller than firstNumber, the result shall be 0.
+
+*/
+
+public class WhileLoopExercise
+{
+ public static int CalculateSumOfNumbersBetween(int firstNumber, int lastNumber)
+ {
+  if (firstNumber < lastNumber)
+  {
+   return 0;
+  }
+
+  int currentNumber = firstNumber;
+  int sum = 0;
+  while (currentNumber <= lastNumber)
+  {
+   sum += currentNumber;
+   currentNumber++;
+  }
+
+  return sum;
+ }
+
+ /*
+  for firstNumber equal to 5 and lastNumber equal to 10,
+  the result should be 45 because this is the sum of numbers 5, 6, 7, 8, 9, 10
+  */
+
+  /*
+   for firstNumber equal to -5 and lastNumber equal to 5, the result should be 0
+   because this is the sum of numbers -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5
+   */
+
+  /*
+   for firstNumber equal to 10 and lastNumber also equal to 10, the result should be 10
+   because, to the sum, we include the only number, which is 10
+   */
+
+  /*
+   for firstNumber equal to 5 and lastNumber equal to 4,
+   the result should be 0 because the lastNumber is smaller than firstNumber
+   */
+ }    
