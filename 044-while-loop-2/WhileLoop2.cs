f4e1248ab160a7e9163213dfd6cd8eb04b29fd36
valatwork <valfuindor@outlook.com>
2024-11-20 11:42:20 +0100

    Empty commit

---
+++ 044-while-loop-2/WhileLoop2.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter a word");
+var userInput = Console.ReadLine();
+
+while (userInput.Length < 15)
+{
+    userInput += 'a';
+    //userInput = userInput + 'a';
+    Console.WriteLine(userInput);
+}
+
+Console.WriteLine("The loop is finished");
+Console.ReadKey();
