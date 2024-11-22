590c16f73f1772ec94c1e4c75a2c18435fae8a9f
valatwork <valfuindor@outlook.com>
2024-11-22 16:01:44 +0100

    Empty commit

---
+++ 045-do-while-loop/DoWhileLoop.cs
@@ -0,0 +1,39 @@
+﻿WordLoop1();
+WordLoop2();
+WordLoop3();
+void WordLoop1()
+{
+    string word;
+    do
+    {
+        Console.WriteLine("Enter a word longer than 10 letters");
+        word = Console.ReadLine();
+    } while (word.Length <= 10);
+
+    Console.WriteLine("The loop is finished");
+    Console.ReadKey();
+}
+
+void WordLoop2()
+{
+    string word = " ";
+    while (word.Length <= 10)
+    {
+        Console.WriteLine("Enter a word longer than 10 letters");
+        word = Console.ReadLine();
+    }
+    Console.WriteLine("The loop is finished");
+    Console.ReadKey();
+}
+
+void WordLoop3()
+{
+    string word = "12345";
+    while (word.Length >= 3)
+    {
+        Console.WriteLine("Enter a word longer than 10 letters");
+        word = Console.ReadLine();
+    }
+    Console.WriteLine("The loop is finished");
+    Console.ReadKey();
+}
---
+++ 046-for-loop/Program.cs
@@ -0,0 +1,7 @@
+﻿for (int i = 0; i < 5; ++i)
+{
+    Console.WriteLine("Hello");
+}
+
+Console.WriteLine("The loop is finished");
+Console.ReadKey();
