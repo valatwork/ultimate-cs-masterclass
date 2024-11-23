2705ee11629cec254170349ef99a3d50c7ce468b
valatwork <valfuindor@outlook.com>
2024-11-23 12:37:57 +0100

    Empty commit

--- 046-for-loop/Program.cs
+++ 046-for-loop/Program.cs
@@ -1,7 +1,23 @@
-﻿for (int i = 0; i < 5; ++i)
+﻿ForLoop1();
+Factorial();
+
+void ForLoop1()
 {
-    Console.WriteLine("Hello");
+    for (int i = 0; i < 5; ++i)
+    {
+        Console.WriteLine("Hello");
+    }
+    Console.WriteLine("The loop is finished");
 }
 
-Console.WriteLine("The loop is finished");
+static int Factorial(int number)
+{
+    int result = 1;
+    for (int i = 1; i <= 5; ++i)
+    {
+        result *= i;
+    }
+    Console.WriteLine("Factorial is finished")
+}
+
 Console.ReadKey();
---
+++ 047-break/Program.cs
@@ -0,0 +1,44 @@
+﻿BreakLoop001();
+BreakLoop002();
+BreakLoop003();
+
+void BreakLoop001()
+{ for (var i = 0; i < 100; i++)
+    {
+        Console.WriteLine("i is " + i);
+        if (i > 25)
+        {
+            break;
+        }
+    }
+    
+}
+
+void BreakLoop002()
+{
+    int userNumber;
+    do
+    {
+        Console.WriteLine("Enter a number larger than 20: ");
+        var userInput = Console.ReadLine();
+        userNumber = int.Parse(userInput);
+    } while (userNumber <= 20);
+}
+
+void BreakLoop003()
+{
+    int userNumber;
+    do
+    {
+        Console.WriteLine("Enter a number larger than 10: ");
+        var userInput = Console.ReadLine();
+
+        if (userInput == "stop")
+        {
+            break;
+        }
+        userNumber = int.Parse(userInput);
+    } while (userNumber <= 10);
+}
+
+Console.ReadKey();
